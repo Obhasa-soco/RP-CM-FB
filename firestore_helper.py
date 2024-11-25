bbf920907a97f510f4aaeceddc6ccfece3dca3a5
Obhasa <obhasa.putra@sociolla.com>
2024-11-25 16:07:46 +0700

    Update secret code

--- firestore_helper.py
+++ firestore_helper.py
@@ -1,5 +1,10 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env file
+load_dotenv()
 
 class FirebaseDB:
     def __init__(self, service_account_key_path="/home/soco/Projects/Claw_machine_firebase/serviceAccountKey.json"):
@@ -14,18 +19,20 @@ class FirebaseDB:
         firebase_admin.initialize_app(cred)
 
     def find_and_delete_by_id(self, collection_name, document_id):
+        # Secret Code
+        SECRET = os.getenv("THIS_IS_SECRET")
         # Reference the document by ID
         doc_ref = self.db.collection(collection_name).document(document_id)
         
         # Fetch the document
         doc = doc_ref.get()
 
-        if doc.exists and document_id != "socoevnt24":
+        if doc.exists and document_id != SECRET:
             # Delete the document
             doc_ref.delete()
             print(f"Document with ID {document_id} has been deleted.")
             return True
-        elif doc.exists and document_id == "socoevnt24":
+        elif doc.exists and document_id == SECRET:
             print("Super User$$")
             return True
         else:
